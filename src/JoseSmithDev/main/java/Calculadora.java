d9122daa146674aaad079898e5ec5f568484917a
JoseSmithDev <josesmithdev@gmail.com>
2021-02-10 20:01:27 +0100

    - Añadidas funcionalidades de Suma y resta!

--- src/JoseSmithDev/main/java/Calculadora.java
+++ src/JoseSmithDev/main/java/Calculadora.java
@@ -3,4 +3,11 @@ public class Calculadora {
     public int suma(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int restar(int num1, int num2){
+        return num1 - num2;
+    }
+    public int mult (int num1, int num2){
+        return num1 * num2;
+    }
 }
