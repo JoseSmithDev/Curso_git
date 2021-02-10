f2189cdc1b1b8b3b3aa03d0b34ab97adb6f0e2fa
JoseSmithDev <josesmithdev@gmail.com>
2021-02-10 19:48:12 +0100

    - Creacion de la clase Claculadora - Eliminacion de la Clase Main - Modificacion del fichero gitignore

--- src/JoseSmithDev/main/java/Main.java
+++ src/JoseSmithDev/main/java/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Hola Mundo");
     }
+
+    public static int suma(int num1, int num2){
+        return num1 + num2;
+    }
+
 }
